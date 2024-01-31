6acb0bc748cde90877e3af1cbc4053564b971e72
Sancrum9 <158079317+Sancrum9@users.noreply.github.com>
2024-01-31 17:18:30 +0000

    Fried Chicken Recipe

---
+++ kickoff warmup.py
@@ -0,0 +1,12 @@
+"I think computer program is something something thats a specific human being created and only him or her is able to altar."
+"Computer programs are very useful and we use it everyday."
+#What percent of the world can go 1 day without using computer programs
+#My favorite food is fried chicken and no not because im black
+"How to make fried Chicken"
+"STEP1: Defrost your chicken"
+"STEP2: Clean your Chicken"
+"STEP3: Season your chicken and no not just with no salt and pepper what I look like"
+"STEP4: Crack Egss into a bowl"
+"STEP5: Pour Flour into a plate"
+"STEP6: Submerge chicken into eggs and flower 3x"
+"STEP7: Get a pot pour vegetable oil and then heat it up to about 350 degrees And put your chicken in an dthen in about a couple of minutes its done"
