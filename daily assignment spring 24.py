1174a4b6e3f08a69be2ef420012b34aacd2702c6
Sancrum9 <158079317+Sancrum9@users.noreply.github.com>
2024-02-01 18:15:48 +0000

    work

---
+++ daily assignment spring 24.py
@@ -0,0 +1,52 @@
+#age=13
+#print("you are "+ str(age) + " years old")
+#print("you are", age, "years old")
+#print(f"you are {age} years old")
+
+#age= 31
+#players= 2
+#quantity= 3
+
+#print(f"you are {age} years old")
+#print(f"there are {players} players online")
+#print(f"you would like to buy {quantity} items")
+
+#gpa= 4.0
+#distance= 2.5
+#price= 10.99
+
+#print(f"your gpa is {gpa} ")
+#print(f"you walked {distance} kilomiters")
+#print(f"you owe me ${price}")
+
+#name= "ryder"
+#food= "pizza"
+#email= "rbrown890@gmail.com"
+
+#print(f"my email is {email} I like to eat {food} and my name is {name} and im also a turkey")
+
+#online= True
+#for_sale= True
+#running= False
+
+#if online:
+  #  print("the game is running")
+#else:
+  #  print("the game is over")
+
+#if for_sale:
+#    print("Item approved")
+#else:
+ #   print("unable to purchase item")
+#if running:
+  #  print("can dash")
+#else:
+   # print("unable to dash")
+
+
+name= ("Sa'id")
+age= 13
+print(f"Hi my name is {name} I am {13} years old i feel as if variable are very useful and mostly becuase of the way the work im so fastinated by the process of coding that everything I do that makes me so fastinated variables are like hidden messages that i dont have to text")
+ gpa= 1.500
+ school= ("boyslatin")
+ print(f"Hi my name is {name} im {age} my gpa is {gpa} I go to {school} in this sentence I used float and and intigers in this sentence and a balean")
