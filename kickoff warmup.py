57dab522064ae8829c59de0edd8ceb2305ca2509
Sancrum9 <158079317+Sancrum9@users.noreply.github.com>
2024-02-05 16:29:47 +0000

    uhh i fixed stuff

--- kickoff warmup.py
+++ kickoff warmup.py
@@ -2,7 +2,7 @@
 "Computer programs are very useful and we use it everyday."
 #What percent of the world can go 1 day without using computer programs
 #My favorite food is fried chicken and no not because im black
-"How to make fried Chicken"
+(f"How to make fried Chicken")
 "STEP1: Defrost your chicken"
 "STEP2: Clean your Chicken"
 "STEP3: Season your chicken and no not just with no salt and pepper what I look like"
