717cc22066d4257788496540da6da386b97ba0d0
Sancrum9 <158079317+Sancrum9@users.noreply.github.com>
2024-02-06 17:54:50 +0000

    notes

---
+++ activityFeb6.py
@@ -0,0 +1,23 @@
+#what is complexity
+# Complexity: Is the measure of recources a program uses.
+#Resorces = time, data, energy
+
+#What is abstraction
+#The process of simplifying or hiding complexity
+
+# What is a variable
+# A container for data
+
+# A variable must start with a letter or a underscore
+# VArible names are short 
+# = x=5
+# += x+=5
+#-= x-=3
+#*= x*=3
+#/= x/=3
+
+#Comperison operators= comperison action
+crateofpies=12
+crateofapples=28
+
+print(crateofpies <= crateofapples)
