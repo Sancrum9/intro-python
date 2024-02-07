cba80f95d22867338916213573c8009a656f1b5a
Sancrum9 <158079317+Sancrum9@users.noreply.github.com>
2024-02-07 17:31:54 +0000

    finisheddailyactivity

---
+++ activityFeb7.py
@@ -0,0 +1,15 @@
+print(f"I think computer program is something something thats a specific human being created and only him or her is able to altar.")
+
+#1.Booleans
+#2.String
+#3.Integers
+
+#1.Arathmitic
+#2.Comperison
+#3.Logical
+
+#A variable is a place to store iformation
+
+#I would use a logical operator if the user have all the thing becaususe logical deals with 2 or more things then this give them access
+#I would use Assignment operater because after it adds the prices all together it will automaticlly deduct 20%
+#I would use comparison to figure out the id situation if this his age True does he have the same dob True
