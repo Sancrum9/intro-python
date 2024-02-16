2a7e4493799dcdaae90dff7ddf9b3d27042d4460
Sancrum9 <158079317+Sancrum9@users.noreply.github.com>
2024-02-16 17:32:05 +0000

    donethisquizz

---
+++ saidsQuizz.py
@@ -0,0 +1,63 @@
+#1. What built-in function would you use if you wanted
+# to print out data to a user?
+print(f"the print frunction")
+#2. What built-in function would you use if you wanted 
+# a user to pass in data to a program?
+print(f" it would be input")
+#3. Explain what complexity and abstraction are, then 
+# provide an example describing their relationship to one another.
+# You cannot use the following examples because they 
+# were shared in class; grocery/ electronics store, car, phone. 
+
+#4. What does it mean when you encounter a syntax error?
+print(f"when you incounter a syntax error it mean you aren't speaking the computers language")
+#5. What is a variable? Provide an example of a variable in code.
+print(f"a stroage container")
+#6. Write a function in Python that when called will print out 
+# your name.
+name=("Sa'id")
+print(f"hi my name is {name}")
+#7. What is a function definition and a function call?
+
+#8. Describe what each symbol means?
+
+# = // single equal
+# == // double equal
+# != // exclaimation equal
+print(f"single means its a normal math equation 2 equales wich one is true or false like if there bot equale to and a exclamation mark means its not equal too ")
+#9. What will the output for the following code block be?
+var1 = 1
+var2 = 2
+var3 = "3"
+
+#print(var1 + var2 + var3)
+
+print(f'it would be a type error because var3 is a string so you would have to put' +str(var3))
+#10. What will the output for the following code block be?
+listOne = [20, 40, 60, 80]
+listTwo = [20, 40, 60, 80]
+
+print(listOne == listTwo)
+print(f" they would say true because it has 2 equal signs which means it's gonna determine if the 2 list are even or not")
+# 11. Create the following python lists and include the following
+# data types for each list:
+# a- create a python list of strings. The strings should be items you
+# can find in a book bag.
+# b- Create a python list of floats. the floats should represent the 
+# price of each item in a book bag from the previous list.
+listone= ["pencils, paper, notebook,"]
+listtwo= [12.50, 50.00, 15.00]
+print(listone != listtwo)
+# 12. What are the Three (3) naming formats we use for naming variables, files, and functions?
+
+# 13. Provide a coding scenario where you would you use a comparison operator in a program?
+print(f"say get in this place everything that u give the worker there would have to match with what they got on thier data base")
+# 14. Provide a coding scenario Where you would you use a logical operator in a program?
+print(f'say u and your friends have to get something to eat and ya need to buy something from the store so ya''ll combine yall money so ya can get the pizza')
+# 15. Create a block of code where you would 
+# use a comparison operator with the follwing data:
+currentScore= 100
+highScore=2300
+print(currentScore==highScore)
+# Bonus : What does the type() function do?
+print(f"The type() function returns the type of the specified object")
