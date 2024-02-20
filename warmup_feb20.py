9ed26ced66c75497ec32022351abda6d62a8ff34
Sancrum9 <158079317+Sancrum9@users.noreply.github.com>
2024-02-20 18:00:00 +0000

    y

---
+++ warmup_feb20.py
@@ -0,0 +1,12 @@
+laundryitems= ['clothes',' bascket',' bags','detergant','softner']
+def laundryindex():
+    print(laundryitems[3])
+    
+laundryindex()
+
+print(len(laundryitems))
+
+def reverseName():
+    userName=input('what is your name? ')[::-1]
+    print(userName)
+reverseName()
