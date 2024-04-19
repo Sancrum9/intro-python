7da12630e1d097fb736477debe37d7010610f0d9
Sancrum9 <158079317+Sancrum9@users.noreply.github.com>
2024-04-19 16:23:19 +0000

    done

---
+++ April19_pythonReview.py
@@ -0,0 +1,39 @@
+#1A.  Visit the following web page and read the article on asking questions.
+# https://www.betterup.com/blog/how-to-ask-good-questions
+
+
+# 1B. Based on what you've read, in your own words, describe what a good
+# question is.
+# what subjects do u find hard based off of your sudying habits
+# 1C. In your opinion and in your own words, why do you think its
+# important to be able to ask good questions.
+# So thats people can get a clearer understaing at wehat your getting at
+#2A. Visit the following webpage and complete the learning style survey
+# http://www.educationplanner.org/students/self-assessments/learning-styles-quiz.shtml
+
+#2B. Based on your completed survey results, which learning style are you? List, three 
+# things from the report that you believe are true. 
+# Sit near the front of the classroom
+#Write down key words, ideas, or instructions.
+# It's OK to tap a pencil, shake your foot, or hold on to something while learning.
+
+#2B. Based on your completed survey results, do you feel as though the result are arcurate in
+# reflecting your learning style, if yes, explain why, if no, explain why. Please write your
+# response in complete sentences. 
+# some stuff they said were true and some stuff werent but for the most case the results were somewha accurate
+
+# 3A. Visit the following webpage and read the article on how to study
+# https://summer.harvard.edu/blog/top-10-study-tips-to-study-like-a-harvard-student/
+
+# 3B. Which of the following tips do you feel you need to work on when studying? Explain
+# why in complete sentences.
+# Cramming I cram a lot of stuff in before a big test or exam
+# I also dont nesicerlily ask for help when i need help for a certain subjects
+
+# 3C. Based on your learning style results, and the arcticle you just read on study tips
+# think about how you can apply the study tips to your learning style. Then, in complete sentences
+# write down four (4) steps you could take to effectively study that is specific you you. 
+# 1.set a specific time to study seperate different subject into different time frames dont forget to take breaks
+# 2. Ask teachers for help if your not so sure with certain subjucts
+# 3. If you work  better in groups study with someone 
+# 4. Even if you already learned something review just incase you learn something new
